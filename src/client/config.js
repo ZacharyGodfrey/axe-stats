8e44b89ab375160e69b0c18a3bf672d74e00529c
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-23 15:02:29 -0500

    Add comment

--- src/client/config.js
+++ src/client/config.js
@@ -1,10 +1,12 @@
 const {
   NODE_ENV: environment,
-  PORT: port
+  PORT: port,
+  URL: currentUrl
 } = process.env;
 
 module.exports = {
   environment,
+  currentUrl,
   baseUrl: {
     production: 'https://axecharts.com',
     local: `http://localhost:${port || 8080}`,
