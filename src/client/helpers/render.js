5a2edab8db5b1d43bd86be3cccc75b4640ef55e9
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-23 09:25:24 -0500

    Fix content path

--- src/client/helpers/render.js
+++ src/client/helpers/render.js
@@ -1,7 +1,8 @@
 const fs = require('fs-extra');
+const path = require('path');
 const { render } = require('mustache');
 
-const layout = fs.readFileSync(require.resolve('./_shell.html'), 'utf-8');
+const layout = fs.readFileSync(path.resolve(__dirname, '../pages/_shell.html'), 'utf-8');
 
 module.exports = ({ page, data, partials }) => {
   return render(layout, data, {
