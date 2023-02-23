0f3353ff5ed0dbe1c4b28c070cbec416af49e373
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-23 11:49:52 -0500

    Fix build

--- _scripts/build.js
+++ _scripts/build.js
@@ -1,11 +1,13 @@
 const path = require('path');
 const fs = require('fs-extra');
 
-const client = path.resolve(__dirname, '../src/client');
-const dist = path.resolve(__dirname, '../dist');
+const client = require('../src/client');
+
+const clientDir = path.resolve(__dirname, '../src/client');
+const distDir = path.resolve(__dirname, '../dist');
 
 (async () => {
-  await fs.emptyDir(dist);
-  await fs.copy(`${client}/static`, dist);
-  await fs.outputFile(`${dist}/404.html`, await client['not-found'](), 'utf-8');
+  await fs.emptyDir(distDir);
+  await fs.copy(`${clientDir}/static`, distDir);
+  await fs.outputFile(`${distDir}/404.html`, await client['not-found'](), 'utf-8');
 })();
