b29347808b565ca51f6dac208222db1a6aadc918
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-23 15:08:58 -0500

    Update config

--- src/client/config.js
+++ src/client/config.js
@@ -1,11 +1,7 @@
 const {
-  NODE_ENV: environment,
-  PORT: port,
-  URL: currentUrl
+  URL: baseUrl
 } = process.env;
 
 module.exports = {
-  environment,
-  currentUrl,
-  baseUrl: currentUrl || `http://localhost:${port || 8080}`
+  baseUrl
 };
