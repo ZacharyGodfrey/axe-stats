5a44e946fd39899d4d38d75ff0cccee9e00fe765
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-24 16:18:48 -0500

    Data

--- src/database/data/throwers.js
+++ src/database/data/throwers.js
@@ -2,12 +2,14 @@ module.exports = [
   {
     id: '123',
     name: 'REDACTED',
+    rank: 31,
     standardRating: 1509,
     premierRating: 1600
   },
   {
     id: '234',
     name: 'Beginner',
+    rank: 237,
     standardRating: 1200,
     premierRating: 1050
   }
