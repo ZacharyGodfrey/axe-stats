3f3fb6a299a3e0aa96a3a9f0f347a28f10da5a24
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-24 17:28:17 -0500

    Data

--- src/database/data/throwers.js
+++ src/database/data/throwers.js
@@ -1,16 +1,9 @@
-module.exports = [
-  {
-    id: '123',
-    name: 'REDACTED',
-    rank: 31,
-    standardRating: 1509,
-    premierRating: 1600
-  },
-  {
-    id: '234',
-    name: 'Beginner',
-    rank: 237,
-    standardRating: 1200,
-    premierRating: 1050
+module.exports = Array.from({ length: 25 }).fill().map((_, index) => {
+  return {
+    id: 100 + index,
+    name: `Thrower ${index + 1}`,
+    rank: index + 1,
+    standardRating: 1500 - (index * 10),
+    premierRating: 1600 - (index * 8)
   }
-];
+});
