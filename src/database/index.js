c4285c6b11eb5eb013d5382157c38751534f684d
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-02-28 10:02:42 -0500

    Fix database export

--- src/database/index.js
+++ src/database/index.js
@@ -2,54 +2,52 @@ const sqlite3 = require('sqlite3').verbose();
 
 const FILE_NAME = './data.db';
 
-module.exports = () => {
-  let connection = null;
+const topThrowers = require('./queries/top-throwers');
+const getThrowerById = require('./queries/get-thrower-by-id');
 
-  const topThrowers = require('./queries/top-throwers');
-  const getThrowerById = require('./queries/get-thrower-by-id');
+let connection = null;
 
-  const interface = {
-    topThrowers: () => topThrowers.apply(interface, arguments),
-    getThrowerById: () => getThrowerById.apply(interface, arguments),
-    _connection: () => connection,
-    connect: () => {
-      connection = connection || new sqlite3.Database(FILE_NAME, (error) => {
-        if (error) {
-          throw error;
-        } else {
-          console.log('Connected to the databse.');
-        }
-      });
-    },
-    query: (sql, params = []) => {
-      if (!connection) {
-        throw new Error('Not connected to the database.');
+const db = {
+  topThrowers: () => topThrowers.apply(db, arguments),
+  getThrowerById: () => getThrowerById.apply(db, arguments),
+  _connection: () => connection,
+  connect: () => {
+    connection = connection || new sqlite3.Database(FILE_NAME, (error) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log('Connected to the databse.');
       }
-
-      let results = [];
-
-      connection.all(sql, params, (error, rows) => {
-        if (error) {
-          throw error;
-        } else {
-          results = rows;
-        }
-      });
-
-      return results;
-    },
-    disconnect: () => {
-      connection.close((error) => {
-        if (error) {
-          throw error;
-        } else {
-          console.log('Disconnected from the database.')
-        }
-      });
-
-      connection = null;
+    });
+  },
+  query: (sql, params = []) => {
+    if (!connection) {
+      throw new Error('Not connected to the database.');
     }
-  };
 
-  return interface;
+    let results = [];
+
+    connection.all(sql, params, (error, rows) => {
+      if (error) {
+        throw error;
+      } else {
+        results = rows;
+      }
+    });
+
+    return results;
+  },
+  disconnect: () => {
+    connection.close((error) => {
+      if (error) {
+        throw error;
+      } else {
+        console.log('Disconnected from the database.')
+      }
+    });
+
+    connection = null;
+  }
 };
+
+module.exports = db;
