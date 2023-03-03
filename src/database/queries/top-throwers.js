ddb40e17f06921f6f4bdb7addb9d0f2d3433b760
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-03 08:58:07 -0500

    Add log

--- src/database/queries/top-throwers.js
+++ src/database/queries/top-throwers.js
@@ -1,6 +1,8 @@
 module.exports = (db, count, type) => {
   const sql = `SELECT * FROM profiles ORDER BY ? DESC LIMIT ?`;
 
+  console.log(`Top Throwers: ${count} ${type}`);
+
   return db.query(sql, [
     type === 'premier' ? 'premierRating' : 'standardRating',
     count
