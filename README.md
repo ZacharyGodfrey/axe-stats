cd07926ad5f9e214374edf64967a5ea024e2d512
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-03 14:49:07 -0500

    Add Puppeteer to scrape

--- README.md
+++ README.md
@@ -20,16 +20,6 @@ Axe Charts is a simple site for visualizing IATF axe throwing data
 
 ## References
 
-https://docs.netlify.com/configure-builds/file-based-configuration
-
-https://docs.netlify.com/site-deploys/overview/#deploy-contexts
-
-https://github.com/AaronBeaudoin/vite-plugin-ssr-example-netlify
-
-https://github.com/vuejs/petite-vue
-
 https://pptr.dev
 
 https://techoverflow.net/2019/08/15/minimal-puppeteer-response-interception-example
-
-https://www.sqlitetutorial.net/sqlite-select
--- _scripts/scrape.js
+++ _scripts/scrape.js
@@ -1,7 +1,17 @@
 const fs = require('fs-extra');
+const puppeteer = require('puppeteer');
+
 const db = require('../src/database');
 
-const ensureSchema = async (db) => {
+const deleteDatabase = async () => {
+  console.log(`[SCRAPE] Delete Database: ${db._fileName}`);
+
+  await fs.remove(db._fileName);
+};
+
+const ensureSchema = async () => {
+  console.log('[SCRAPE] Ensure Schema');
+
   await db.query(`
     CREATE TABLE IF NOT EXISTS timestamp (
       id INTEGER PRIMARY KEY,
@@ -12,27 +22,29 @@ const ensureSchema = async (db) => {
   await db.query(`
     CREATE TABLE IF NOT EXISTS profiles (
       id INTEGER PRIMARY KEY,
-      urlId TEXT NOT NULL UNIQUE,
       name TEXT NOT NULL UNIQUE,
-      standardRating INTEGER DEFAULT 0,
-      premierRating INTEGER DEFAULT 0
-    );
+      standardRank INTEGER NOT NULL,
+      standardRating INTEGER NOT NULL,
+      premierRank INTEGER NOT NULL,
+      premierRating INTEGER NOT NULL
+    ) WITHOUT ROWID;
   `);
 
   await db.query(`
     CREATE TABLE IF NOT EXISTS seasons (
       id INTEGER PRIMARY KEY,
-      profileId INTEGER NOT NULL,
       urlId TEXT NOT NULL,
       name TEXT NOT NULL,
       ruleset TEXT NOT NULL,
       date TEXT NOT NULL,
       FOREIGN KEY (profileId) REFERENCES profiles (id) ON DELETE CASCADE ON UPDATE NO ACTION
-    );
+    ) WITHOUT ROWID;
   `);
 };
 
-const seedTables = async (db) => {
+const seedTables = async () => {
+  console.log('[SCRAPE] Seed Tables');
+
   const now = new Date().toISOString();
 
   const me = [
@@ -59,27 +71,113 @@ const seedTables = async (db) => {
 
   await db.query(`INSERT INTO timestamp (timestamp) VALUES(?);`, [now]);
 
-  const [profile] = await db.query(`
-    INSERT INTO profiles (urlId, name, standardRating, premierRating)
-    VALUES (?, ?, ?, ?) RETURNING *;
-  `, me);
+  // const [profile] = await db.query(`
+  //   INSERT INTO profiles (urlId, name, standardRating, premierRating)
+  //   VALUES (?, ?, ?, ?) RETURNING *;
+  // `, me);
 
-  await Promise.all(seasons.map(season => db.query(`
-    INSERT INTO seasons (profileId, urlId, name, ruleset, date)
-    VALUES (?, ?, ?, ?, ?);
-  `, [profile.id, ...season])));
+  // await Promise.all(seasons.map(season => db.query(`
+  //   INSERT INTO seasons (profileId, urlId, name, ruleset, date)
+  //   VALUES (?, ?, ?, ?, ?);
+  // `, [profile.id, ...season])));
+};
+
+const scrape = async () => {
+  console.log('[SCRAPE] Begin Browser Operations');
+
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  const timeout = 10 * 1000;
+
+  await page.goto('https://axescores.com/players/collins-rating');
+
+  await page.waitForResponse(responseHandler({
+    status: 200,
+    method: 'GET',
+    url: 'https://api.axescores.com/players',
+    handler: playersHandler
+  }), { timeout });
+
+  await browser.close();
+};
+
+const responseHandler = ({ status, method, url, handler }) => async (response) => {
+  if (response.status() !== status) return false;
+  if (response.request().method() !== method) return false;
+  if (response.url() !== url) return false;
+
+  const data = response.json();
+
+  return handler(data).then(() => true).catch(() => false);
+};
+
+const playersHandler = async ({ ratingsCategories }) => {
+  console.log('[SCRAPE] Handle Players Data');
+
+  const { players: standard } = ratingsCategories['IATF Standard'];
+  const { players: premier } = ratingsCategories['IATF Premier'];
+  const profilesById = {};
+
+  console.log(`[SCRAPE] Found ${standard.length} Standard Players`);
+  console.log(`[SCRAPE] Found ${premier.length} Premier Players`);
+
+  standard.forEach(({ id, name, rank, rating }) => {
+    profilesById[id] = profilesById[id] || {
+      id,
+      name,
+      standard: {},
+      premier: {}
+    };
+
+    const stats = profilesById[id].standard;
+
+    stats.rank = rank;
+    stats.rating = rating;
+  });
+
+  premier.forEach(({ id, name, rank, rating }) => {
+    profilesById[id] = profilesById[id] || {
+      id,
+      name,
+      standard: {},
+      premier: {}
+    };
+
+    const stats = profilesById[id].premier;
+
+    stats.rank = rank;
+    stats.rating = rating;
+  });
+
+  const uniqueProfiles = Object.values(profilesById);
+
+  console.log(`[SCRAPE] Found ${uniqueProfiles.length} Unique Profiles`);
+
+  await Promise.all(uniqueProfiles.map(async (profile) => {
+    const params = [
+      profile.id,
+      profile.name,
+      profile.standard.rank || 0,
+      profile.standard.rating || 0,
+      profile.premier.rank || 0,
+      profile.premier.rank || 0,
+    ];
+
+    await db.query(`
+      INSER INTO profiles (id, name, standardRank, standardRating, premierRank, premierRating)
+      VALUES (?, ?, ?, ?, ?, ?);
+    `, params);
+  }));
 };
 
 (async () => {
   try {
-    console.log('Deleting database file before connecting...');
+    await deleteDatabase();
+    await ensureSchema();
+    await seedTables();
+    await scrape();
 
-    fs.removeSync(db._fileName);
-
-    console.log(`Deleted: ${db._fileName}`);
-
-    await ensureSchema(db);
-    await seedTables(db);
+    console.log('[SCRAPE] All steps completed successfully.');
   } catch (error) {
     console.log(error);
 
