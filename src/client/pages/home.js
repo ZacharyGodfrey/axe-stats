7a57d5013d11687bbf24c96ee813aae58ce3d0c4
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-03 15:38:13 -0500

    Change sort order

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -3,8 +3,14 @@ const { readFile, render } = require('../helpers');
 const page = readFile(`${__dirname}/../content/home.html`);
 
 module.exports = async (db) => {
-  const allProfiles = await db.allProfiles();
   const updatedAt = await db.timestamp();
+  const allProfiles = await db.query(`
+    SELECT *
+    FROM profiles
+    ORDER BY
+      premierRank DESC,
+      standardRank DESC;
+  `);
 
   return render(page, { allProfiles, updatedAt });
 };
