e72fb5a01be3dd6ec1ef46f0162d03f82b000e67
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-08 16:16:37 -0500

    Content

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -3,14 +3,35 @@ const { readFile, render } = require('../helpers');
 const page = readFile(`${__dirname}/../content/home.html`);
 
 module.exports = async (db) => {
-  const updatedAt = await db.timestamp();
-  const allProfiles = await db.query(`
-      SELECT *
-      FROM profiles
-      WHERE premierRank > 0
-      ORDER BY premierRank ASC, premierAverage DESC
-      LIMIT 256;
-  `);
-
-  return render(page, { allProfiles, updatedAt });
+  return render(page, {
+    updatedAt: await db.timestamp(),
+    standard: {
+      average: await db.query(`
+        SELECT ROUND(AVG(standardAverage), 3)
+        FROM profiles
+        WHERE standardAverage > 0;
+      `),
+      top256: await db.query(`
+        SELECT *
+        FROM profiles
+        WHERE standardRank > 0
+        ORDER BY standardRank ASC, standardAverage DESC
+        LIMIT 256;
+      `)
+    },
+    premier: {
+      average: await db.query(`
+        SELECT ROUND(AVG(premierAverage), 3)
+        FROM profiles
+        WHERE premierAverage > 0;
+      `),
+      top256: await db.query(`
+        SELECT *
+        FROM profiles
+        WHERE premierRank > 0
+        ORDER BY premierRank ASC, premierAverage DESC
+        LIMIT 256;
+      `)
+    }
+  });
 };
