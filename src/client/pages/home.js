2a65d895edc5042d3bb83cdfc3fd07ac69b39b67
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-08 15:05:06 -0500

    Update home.js

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -5,9 +5,11 @@ const page = readFile(`${__dirname}/../content/home.html`);
 module.exports = async (db) => {
   const updatedAt = await db.timestamp();
   const allProfiles = await db.query(`
-    SELECT *
-    FROM profiles
-    WHERE name LIKE '%REDACTED%';
+      SELECT *
+      FROM profiles
+      WHERE premierRank > 0
+      ORDER BY premierRank ASC, premierAverage DESC
+      LIMIT 256;
   `);
 
   return render(page, { allProfiles, updatedAt });
