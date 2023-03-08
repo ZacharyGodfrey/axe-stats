8ecead05acb9b14da339c0a8a454d463979f1767
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-08 12:56:19 -0500

    Add profile page to client interface

--- src/client/index.js
+++ src/client/index.js
@@ -2,5 +2,6 @@ module.exports = {
   'home': require('./pages/home'),
   '404': require('./pages/404'),
   '500': require('./pages/500'),
+  'profile': require('./pages/profile'),
   'comparison': require('./pages/comparison'),
 };
