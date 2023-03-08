60e02fdad7bb8df8a1bcb951fc977d050479a085
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-08 16:48:21 -0500

    Content

--- src/client/pages/home.js
+++ src/client/pages/home.js
@@ -5,6 +5,7 @@ const page = readFile(`${__dirname}/../content/home.html`);
 module.exports = async (db) => {
   return render(page, {
     updatedAt: await db.timestamp(),
+    totalProfiles = (await db.get(`SELECT COUNT(*) AS "count" FROM profiles;`)).count,
     standard: {
       average: (await db.get(`
         SELECT ROUND(AVG(standardAverage), 3) AS "average"
