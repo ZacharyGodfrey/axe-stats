8fe9265791fc50bdb0a0199bd276b2323a38c28a
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-03-09 07:39:57 -0500

    Content

--- src/client/pages/profile.js
+++ src/client/pages/profile.js
@@ -3,6 +3,7 @@ const { readFile, render } = require('../helpers');
 const page = readFile(`${__dirname}/../content/profile.html`);
 
 module.exports = async (profile, updatedAt) => render(page, {
+  title: profile.name,
   profile,
   updatedAt
 });
