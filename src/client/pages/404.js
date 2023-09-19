c6020e38647f65d1ad7a6d235440cd57d8f6d81d
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-09-19 12:47:14 -0400

    WIP

--- src/client/pages/404.js
+++
@@ -1,5 +0,0 @@
-const { readFile, render } = require('../helpers');
-
-const page = readFile(`${__dirname}/../content/404.html`);
-
-module.exports = () => render(page, { title: 'Not Found' });
--- src/scripts/build.js
+++ src/scripts/build.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const fs = require('fs-extra');
+const { render } = require('mustache');
 
 const DB = require('../helpers/database');
 
@@ -17,28 +18,52 @@ const getProfiles = () => {
   `);
 };
 
-const writeFile = (name, content) => {
-  const filePath = `${DIST_DIR}/${name}`;
+const readFile = (filePath) => {
+  return fs.readFile(filePath, 'utf-8');
+};
 
+const writeFile = (filePath, content) => {
   console.log(`Write File: ${filePath}`);
 
   return fs.outputFile(filePath, content, 'utf-8');
 };
 
-const buildIndexPage = async (profiles) => {
-  //
+const buildHomePage = async (shell, profiles) => {
+  const page = await readFile(`${CLIENT_DIR}/home.html`);
+  const data = {
+    title: undefined,
+    profiles
+  };
+
+  return render(shell, data, { page });
 };
 
-const build404Page = async () => {
-  //
+const build404Page = async (shell) => {
+  const page = await readFile(`${CLIENT_DIR}/404.html`);
+  const data = {
+    title: 'Not Found'
+  };
+
+  return render(shell, data, { page });
 };
 
-const build500Page = async () => {
-  //
+const build500Page = async (shell) => {
+  const page = await readFile(`${CLIENT_DIR}/500.html`);
+  const data = {
+    title: 'Error',
+  };
+
+  return render(shell, data, { page });
 };
 
-const buildProfilePage = async (profile) => {
-  //
+const buildProfilePage = async (shell, profile) => {
+  const page = await readFile(`${CLIENT_DIR}/profile.html`);
+  const data = {
+    title: profile.name,
+    profile
+  };
+
+  return render(shell, data, { page });
 };
 
 (async () => {
@@ -48,15 +73,18 @@ const buildProfilePage = async (profile) => {
     await fs.emptyDir(DIST_DIR);
     await fs.copy(`${CLIENT_DIR}/static`, DIST_DIR);
 
-    const profiles = await getProfiles();
+    const [profiles, shell] = await Promise.all([
+      getProfiles(),
+      readFile(`${CLIENT_DIR}/shell.html`)
+    ]);
 
     await Promise.all([
-      writeFile('data.json', JSON.stringify(profiles, null, 2)),
-      buildIndexPage(profiles).then(page => writeFile('index.html', page)),
-      build404Page().then(page => writeFile('404.html', page)),
-      build500Page().then(page => writeFile('500.html', page)),
+      writeFile(`${DIST_DIR}/data.json`, JSON.stringify(profiles, null, 2)),
+      buildHomePage(shell, profiles).then(page => writeFile(`${DIST_DIR}/index.html`, page)),
+      build404Page(shell).then(page => writeFile(`${DIST_DIR}/404.html`, page)),
+      build500Page(shell).then(page => writeFile(`${DIST_DIR}/500.html`, page)),
       ...profiles.map(profile => {
-        return buildProfilePage(profile).then(page => writeFile(`${profile.id}/index.html`, page))
+        return buildProfilePage(shell, profile).then(page => writeFile(`${DIST_DIR}/${profile.id}/index.html`, page))
       })
     ]);
   } catch (error) {
