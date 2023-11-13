7499cf0aa142ecd7e39fed1634dee7449729382b
Zachary Godfrey <20821201+ZacharyGodfrey@users.noreply.github.com>
2023-11-13 16:54:30 -0500

    WIP

--- scripts/build-pages.js
+++ scripts/build-pages.js
@@ -113,9 +113,10 @@ const matchText = ({ profileId, matchId, state, outcome, total, rounds }) => {
       ORDER BY rank ASC, rating DESC
     `);
 
+    writeFile(`${DIST_DIR}/about.html`, buildStaticPage(shell, readFile(`${CLIENT_DIR}/about.html`), 'About'));
+    writeFile(`${DIST_DIR}/rating-system.html`, buildStaticPage(shell, readFile(`${CLIENT_DIR}/rating-system.html`), 'Rating System'));
     writeFile(`${DIST_DIR}/404.html`, buildStaticPage(shell, readFile(`${CLIENT_DIR}/404.html`), 'Not Found'));
     writeFile(`${DIST_DIR}/500.html`, buildStaticPage(shell, readFile(`${CLIENT_DIR}/500.html`), 'Error'));
-    writeFile(`${DIST_DIR}/rating-system.html`, buildStaticPage(shell, readFile(`${CLIENT_DIR}/rating-system.html`), 'Rating System'));
     writeFile(`${DIST_DIR}/badges.html`, buildBadgesPage(shell));
     writeFile(`${DIST_DIR}/index.html`, buildHomePage(shell, profiles));
 
